040a14c20e827e5a7ffece62161c937e5fb1d4c1
Simon Krüger <hello@cr0ss.org>
2026-04-27 07:01:55 +0200

    feat: [US-004] developer implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Layout } from './ui/Layout/Layout'
+
 function App() {
   return (
-    <main>
-      <h1>Card Deck v2</h1>
-    </main>
+    <Layout>
+      <h2>Card Deck v2</h2>
+      <p>Welcome to Open Hand</p>
+    </Layout>
   )
 }
 
